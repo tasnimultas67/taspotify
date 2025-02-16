f29541e2333e5b9330d28cafd438f6112d41c8f8
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-02-16 13:42:21 +0600

    Refactor MusicCard component for improved layout and hover effects

--- app/components/MusicCard.js
+++ app/components/MusicCard.js
@@ -2,18 +2,20 @@ import React from "react";
 
 const MusicCard = ({ track }) => {
   return (
-    <div className="bg-slate-900 p-4 rounded-lg">
+    <div className="bg-slate-900 rounded-lg hover:scale-105 transition-all">
       <img
         src={track.image}
         alt={track.name}
-        className="w-full h-48 object-cover rounded-lg mb-4"
+        className="w-full h-48 object-cover rounded-t-lg mb-4"
       />
-      <h2 className="text-xl font-semibold text-white">{track.name}</h2>
-      <p className="text-gray-100">{track.artist_name}</p>
-      <audio controls className="w-full mt-4">
-        <source src={track.audio} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-white">{track.name}</h2>
+        <p className="text-gray-100">{track.artist_name}</p>
+        <audio controls className="w-full mt-4">
+          <source src={track.audio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
     </div>
   );
 };
